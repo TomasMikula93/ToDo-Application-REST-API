06173e8daaaa041d7f0953e424d7107f951f108b
TomasMikula <t.mikula@centrum.cz>
2022-09-12 14:29:43 +0200

    added integration test of UserController

---
+++ src/test/java/com/example/todoapp/Controllers/UserControllerTest.java
@@ -0,0 +1,49 @@
+package com.example.todoapp.Controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void registration() throws Exception {
+        mockMvc.perform(post("/api/registration")// perform action inside POST,GET,DELETE + url. Parameters or path variable can be specified inside. It is string so ""+variable+"" can be used.
+                        .content("""
+                                {
+                                    "username": "Thomas",
+                                    "password": "password123"
+                                }""")
+                        .contentType("application/json"))//you need to specify what type of content is inside of body
+                .andExpect(status().is(200));
+
+    }
+
+    @Test
+    void getUsers() throws Exception {
+        mockMvc.perform(post("/api/registration")// perform action inside POST,GET,DELETE + url. Parameters or path variable can be specified inside. It is string so ""+variable+"" can be used.
+                        .content("""
+                                {
+                                    "username": "Tom",
+                                    "password": "password123"
+                                }""")
+                        .contentType("application/json"))//you need to specify what type of content is inside of body
+                .andExpect(status().is(200));
+
+
+        mockMvc.perform(get("/api/user"))
+          .andExpect(status().is(200));
+
+    }
+}
