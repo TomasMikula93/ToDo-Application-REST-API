f0ad6a1bd46a23a650afa3ed66ff1effe6561edb
TomasMikula <t.mikula@centrum.cz>
2022-09-12 14:46:22 +0200

    added JUnit test for ToDoUserService

--- src/test/java/com/example/todoapp/Services/ToDoUserServiceImplTest.java
+++ src/test/java/com/example/todoapp/Services/ToDoUserServiceImplTest.java
@@ -1,7 +1,33 @@
 package com.example.todoapp.Services;
 
+import com.example.todoapp.Models.ToDoUser;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ToDoUserServiceImplTest {
 
+    private final List<ToDoUser> listOfUsers = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        ToDoUser toDoUser = new ToDoUser("User1", "password", "ROLE_USER");
+        listOfUsers.add(toDoUser);
+    }
+
+    @Test
+    void testFindAllUsers() {
+
+        ToDoUserService toDoUserService = Mockito.mock(ToDoUserService.class);//mocked class using Mockito
+        Mockito.when(toDoUserService.findAllUsers()).thenReturn(listOfUsers);
+
+        assertTrue(toDoUserService.findAllUsers().size() > 0);
+    }
 }
