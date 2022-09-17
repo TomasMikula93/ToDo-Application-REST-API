bc5f4c34983ea3c90bbebdb3f8c64f3896d577f2
TomasMikula <t.mikula@centrum.cz>
2022-09-17 15:31:12 +0200

    email validator implemented

--- src/main/java/com/example/todoapp/Registration/EmailValidation.java
+++ src/main/java/com/example/todoapp/Registration/EmailValidation.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailValidation {
 
-    // TODO finish email validator!
-
     public boolean isValidEmail(String email) {
         EmailValidator validator = EmailValidator.getInstance();
         return validator.isValid(email);
