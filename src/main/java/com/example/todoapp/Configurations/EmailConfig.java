99c8fb1137c0e7419834ad2707eccdbd7b03d6ee
TomasMikula <t.mikula@centrum.cz>
2022-09-21 15:55:46 +0200

    env variables testing

--- src/main/java/com/example/todoapp/Configurations/EmailConfig.java
+++ src/main/java/com/example/todoapp/Configurations/EmailConfig.java
@@ -15,7 +15,7 @@ public class EmailConfig {
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
         mailSender.setUsername(System.getenv("email_username"));
-        mailSender.setPassword(System.getenv("email_password"));
+        mailSender.setPassword(System.getenv("email_pass"));
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
